30e573eae2b849528784c0a9730a1740f07801d1
dashcraft <ashcraftbookkeeping@gmail.com>
2016-08-08 20:17:32 -0500

    Initial commit, yeoman generator used to establish basic app setup and authentication

---
+++ server/routes.js
@@ -0,0 +1,18 @@
+/**
+ * Main application routes
+ */
+
+'use strict';
+
+var path = require('path');
+
+module.exports = function(app) {
+
+  // Insert routes below
+  app.use('/api/things', require('./api/thing'));
+  app.use('/api/users', require('./api/user'));
+
+  app.use('/auth', require('./auth'));
+  
+
+};
