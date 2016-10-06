5ea3c7e20b8eb717f58510e215ffa0cb84798034
dashcraft <ashcraftbookkeeping@gmail.com>
2016-10-06 18:32:52 -0500

    here is some directive stuff beginning working on the navbar

--- public/javascript/controllers/main-controller.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-(function () {
-    'use strict';
-
-    angular.module('app').controller('MainController', ['$scope', function ($scope) {
-        console.log("MainController functional!");
-    }]);
-})();
