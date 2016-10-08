589f217c46c2bf5f7653549a203121ed35f85e46
Ubuntu <ubuntu@ip-172-31-31-203.us-west-2.compute.internal>
2016-10-08 21:41:11 +0000

    simple script to check if docker images are running and start them if they are not; runs on crontab once per minute

---
+++ keepAlive.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+if [ `docker ps | grep armray-modify | wc -l` == 0 ]; then 
+    docker run -p 1152:1152 armray-modify &
+fi
+
+if [ `docker ps | grep armray-retrieve | wc -l` == 0 ]; then
+    docker run -p 1151:1151 armray-retrieve &
+fi
+
+if [ `docker ps | grep armray-modify | wc -l` == 0 ]; then
+    docker run -p 1150:1150 armray-search &
+fi
+
+
