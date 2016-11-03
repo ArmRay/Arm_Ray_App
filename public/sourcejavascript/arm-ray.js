147fa384d798cbe39493cd150a032fd987bd1bfa
Sean Ivins <sean42ivins@gmail.com>
2016-11-03 20:36:34 +0000

    Fixed button to be flat

--- public/sourcejavascript/arm-ray.js
+++ public/sourcejavascript/arm-ray.js
@@ -81,20 +81,20 @@
         	console.log(user.username,user.password);
         	//login service sends the user object with username and password
         	// it expects a full user object back, and injects it into the next view
-    //     	LoginFactory.save(user,function(data){
-    //         console.log("data successfully saved")
-    //         console.log(data);
-    //         if(data.user._id){
-    //             console.log('user id is not blank');
-    //             // $state.go('admin',{data:user});
-    //             $state.go('dashboard',{data:data.user});
-    //           }
-    //         },
-    //         function(){
-    //         	//There can be a toast popup here
-				// console.log('there was an error');     
-    //         }
-    //       )
+        	LoginFactory.save(user,function(data){
+            console.log("data successfully saved")
+            console.log(data);
+            if(data.user._id){
+                console.log('user id is not blank');
+                // $state.go('admin',{data:user});
+                $state.go('dashboard',{data:data.user});
+              }
+            },
+            function(){
+            	//There can be a toast popup here
+				console.log('there was an error');     
+            }
+          )
         }
 
         
