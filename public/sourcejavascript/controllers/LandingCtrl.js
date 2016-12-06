c985f921e3a217496ad329bb8c6e011cc02f2077
dashcraft <ashcraftbookkeeping@gmail.com>
2016-12-06 14:46:36 -0600

    Merging some new routes and index into front end

---
+++ public/sourcejavascript/controllers/LandingCtrl.js
@@ -0,0 +1,12 @@
+(function () {
+    'use strict';
+
+    angular.module('app').controller('LandingCtrl', ['$scope', function ($scope) {
+        console.log("LandingCtrl functional!");
+
+
+
+
+
+    }]);
+})();
--- public/sourcejavascript/routes.js
+++ public/sourcejavascript/routes.js
@@ -15,28 +15,38 @@
             
             $stateProvider
               .state('login', {
-                  url: "/",
+                  url: "/login",
                   templateUrl: "/views/login.html",
                   controller:"LoginCtrl"   
               }, function(){
                 console.log("Inside of login route");
               })
-
-              .state('dashboard', {
+              .state('index', {
+                  url: "/",
+                  templateUrl: "/views/landing.html",
+                  controller:"LandingCtrl"   
+              }, function(){
+                console.log("Inside of landing page");
+              })
+              .state('app',{
+                  url:"/app",
+                  templateUrl:"/views/app.html",
+              })
+              .state('app.dashboard', {
                   url: "/dashboard",
                   templateUrl: "/views/dashboard.html",
                   controller:"DashboardCtrl"   
               })
-              .state('dashboard.rightProjects',{
-                 templateUrl:"/views/right_project_nav.html",
-                 controller:"rightProjectCtrl"
-              })
-              .state('project', {
+              // .state('app.dashboard.rightProjects',{
+              //    templateUrl:"/views/right_project_nav.html",
+              //    controller:"rightProjectCtrl"
+              // })
+              .state('app.project', {
                   url: "/project",
                   templateUrl: "/views/project.html",
                   controller:"ProjectCtrl"   
               })
-              .state('profile', {
+              .state('app.profile', {
                   url: "/profile",
                   templateUrl: "/views/profile.html",
                   controller:"ProfileCtrl"   
