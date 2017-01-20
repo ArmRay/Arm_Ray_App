d827077640f7775655cd5840e04126f60bef6f2d
dashcraft <ashcraftbookkeeping@gmail.com>
2017-01-20 12:15:50 -0600

    update prod

--- public/sourcejavascript/arm-ray.js
+++ public/sourcejavascript/arm-ray.js
@@ -42,15 +42,11 @@
                   url: "/login",
                   templateUrl: "/views/login.html",
                   controller:"LoginCtrl"   
-              }, function(){
-                console.log("Inside of login route");
               })
               .state('index', {
                   url: "/",
                   templateUrl: "/views/landing.html",
                   controller:"LandingCtrl"   
-              }, function(){
-                console.log("Inside of landing page");
               })
               .state('app',{
                   url:"/app",
@@ -93,8 +89,7 @@
     'use strict';
 
     angular.module('app').controller('LandingCtrl', ['$scope','toastr','$http', function ($scope,toastr,$http) {
-        console.log("LandingCtrl functional!");
-
+        console.log('Are you looking at my console logs? For shame ;)');
         function dangertrigger(){
             toastr.warning('Email has already been registered','Issue!');
         }
@@ -103,8 +98,7 @@
         function checkit(x){
             var check= x;
                 var checkurl = '/api/checkusername/'+check;
-                console.log(check);
-                console.log(checkurl);
+                
                 $http.post(checkurl).then(function(res){
                         if(res.data.exists){
                             console.log('exists exists');
@@ -114,7 +108,7 @@
                             doesemailexist = true;
                         }
                     },function(){   
-                    console.log('there was a checkusername get error');
+
                 })
         }
 
@@ -124,18 +118,18 @@
         
 
         $scope.submitForm = function(email,firstname,lastname){
-        	console.log('this has been clicked');
+
         	if(typeof email != 'undefined' && doesemailexist && typeof firstname != 'undefined' && typeof lastname != 'undefined'){
         		var user = {
         			firstname: firstname,
         			lastname: lastname,
         			email:email
         		}
-        		console.log('Win!')
+
 
         		$http.post('/api/register',user).then(function(data){
  				if(data.message= 'Success'){
- 					console.log('this user was saved succesfully');
+
                     toastr.success('Thank you for registering!');
  					$scope.firstnameTop = '';
  					$scope.lastnameTop = '';
@@ -145,7 +139,7 @@
  					$scope.emailTop = '';
  				}
  				else{
- 					console.log('this user was not saved!');
+
  				}
 
  				if(data.error){
@@ -155,15 +149,15 @@
 
         	})
 
-        	console.log('user',user)
+
         		
         	}
         	else if(typeof email == 'undefined'){
-        		console.log('email was undefined');
+
         		toastr.warning('Your Email was not valid','Invalid!')
         	}
         	else{
-        		console.log('something else was undefined')
+
         		toastr.warning('You must submit a first and last name','Invalid!')
         	}
 
